530c43afa681031cfb4921a1cbae9abf42ec6de9
abdulrahman442 <120608667+abdulrahman442@users.noreply.github.com>
2026-03-18 03:38:00 +0100

    Add tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,1 @@
+export default { content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], theme: { extend: { fontFamily: { sans: ['-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'sans-serif'], }, }, }, plugins: [], }
